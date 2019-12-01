8262629354df4c36d2dd2216005a07cf26651445
Piotr Manczak <51323846+PiotrManczak39@users.noreply.github.com>
2019-12-01 13:22:41 +0000

    Humble beginnings...
    Some of hte new html and JQuery.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-alert('You have been connected!');
+//First text field in focus by default using jQuery.
+$('#name').focus();
+const other = document.getElementsByTagName('input')[2];
+other.style.display = 'none';
