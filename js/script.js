34b2b80b3126f22a1baa89392a3336dd6935a807
Piotr Manczak <51323846+PiotrManczak39@users.noreply.github.com>
2019-12-08 21:15:03 +0000

    Checkboxes
    While unchecking boxes it does not remove disabled state, even though it says so in the console.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 //First text field in focus by default using jQuery.
 $('#name').focus();
 //Hide Other Job Role.
@@ -9,6 +11,10 @@ const $newText = 'Please select a T-shirt theme';
 $('#colors-js-puns label').text($newText);
 //Hide the colors in the “Color” drop down menu.
 $('#color').hide();
+
+/*----------------------------------------------------------
+                 T-shirt Selection
+-----------------------------------------------------------*/
 
 const colorSelection = document.getElementById('color');
 const design = document.getElementById('design');
@@ -42,4 +48,54 @@ design.addEventListener('change', () => {
     steelblue.style.display = 'block';
     dimgrey.style.display = 'block';
   }
+});
+
+/*-----------------------------------------------------------
+               Activities Section
+------------------------------------------------------------*/
+
+let totalActivityCost = 0;
+const totalCost = document.createElement('h3');
+const activitiesDiv = document.querySelector('.activities');
+activitiesDiv.appendChild(totalCost);
+const courseOptions = activitiesDiv.getElementsByTagName('input');
+
+//Event listener checking checkboxes
+activitiesDiv.addEventListener('change', (e) => {
+  if (e.target.tagName === 'INPUT') {
+    let checkbox = e.target;
+    if (checkbox.checked) {
+      //Checking and disabling the same time activities
+      let chosenData = checkbox.getAttribute('data-day-and-time');
+      for (let i=1; i<courseOptions.length; i++) {
+        let data = courseOptions[i].getAttribute('data-day-and-time');
+        if ( data === chosenData ) {
+          if (courseOptions[i] !== checkbox) {
+            courseOptions[i].setAttribute('disabled', true);
+          }
+        }
+      }
+      //Calcutaling total cost of all activities (+)
+      let parent = checkbox.parentNode;
+      let courseCost = checkbox.getAttribute('data-cost');
+      let courseCostNumber = parseInt(courseCost, 10);
+      totalActivityCost += courseCostNumber;
+    } else if (checkbox.checked == false) {
+      //Checking and enabling the same time activities
+      let chosenData = checkbox.getAttribute('data-day-and-time');
+      for (let i=1; i<courseOptions.length; i++) {
+        let data = courseOptions[i].getAttribute('data-day-and-time');
+        if ( data === chosenData && courseOptions[i] !== checkbox) {
+            console.log(courseOptions[i]);
+            courseOptions[i].setAttribute('disabled', false);
+        }
+      }
+      //Calcutaling total cost of all activities (-)
+      let parent = checkbox.parentNode;
+      let courseCost = checkbox.getAttribute('data-cost');
+      let courseCostNumber = parseInt(courseCost, 10);
+      totalActivityCost -= courseCostNumber;
+    }
+  }
+  totalCost.textContent = `Total Cost:  $${totalActivityCost}`;
 });
