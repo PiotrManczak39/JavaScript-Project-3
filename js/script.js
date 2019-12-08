b6f215ff0cf6aff6c02fa43988869925b248dfc6
Piotr Manczak <51323846+PiotrManczak39@users.noreply.github.com>
2019-12-08 13:36:27 +0000

    Tshirts selection set up.

--- js/script.js
+++ js/script.js
@@ -10,17 +10,36 @@ $('#colors-js-puns label').text($newText);
 //Hide the colors in the “Color” drop down menu.
 $('#color').hide();
 
+const colorSelection = document.getElementById('color');
 const design = document.getElementById('design');
 design.addEventListener('change', () => {
+  colorSelection.style.display = 'block';
   let colorTitle = document.querySelector('#colors-js-puns label');
   colorTitle.textContent  = 'Color';
 
-  let puns = document.querySelector('#design :nth-child(2)');
-  if (puns.style.display === 'block') {
-    console.log(puns.textContent);
-  } else {
-    console.log(1);
+  //Options variables
+  let cornflowerblue = colorSelection.querySelector('option:nth-child(1)');
+  let darkslategrey = colorSelection.querySelector('option:nth-child(2)');
+  let gold = colorSelection.querySelector('option:nth-child(3)');
+  let tomato = colorSelection.querySelector('option:nth-child(4)');
+  let steelblue = colorSelection.querySelector('option:nth-child(5)');
+  let dimgrey = colorSelection.querySelector('option:nth-child(6)');
+  //Switching between options
+  if (design.value === 'js puns') {
+    cornflowerblue.selected = 'true';
+    cornflowerblue.style.display = 'block';
+    darkslategrey.style.display = 'block';
+    gold.style.display = 'block';
+    tomato.style.display = 'none';
+    steelblue.style.display = 'none';
+    dimgrey.style.display = 'none';
+  } else if (design.value === 'heart js') {
+    tomato.selected = 'true';
+    cornflowerblue.style.display = 'none';
+    darkslategrey.style.display = 'none';
+    gold.style.display = 'none';
+    tomato.style.display = 'block';
+    steelblue.style.display = 'block';
+    dimgrey.style.display = 'block';
   }
-  let colorSelection = document.getElementById('color');
-  colorSelection.style.display = 'block';
 });
